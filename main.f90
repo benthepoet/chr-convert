8496afec58a6fe66108e2eba9eea487ab3010ae8
Ben Hanna <benpaulhanna@gmail.com>
2021-05-07 01:05:19 -0400

    Fixed output for hexadecimal.

--- main.f90
+++ main.f90
@@ -1,30 +1,39 @@
 program main
   implicit none
 
-  integer :: i, x, y, offset, hi, lo
-  integer, dimension(8) :: pixels
-
-  pixels = (/0, 1, 2, 0, 0, 2, 3, 0/)
-
-  hi = 0
-  lo = 0
+  integer :: i, j, x, y, offset, hi, lo
+  integer, dimension(8, 8) :: pixels
   
-  do i = 1, 8
-     x = pixels(i)
-     offset = 8 - i
+  pixels = transpose(reshape((/0, 0, 0, 0, 0, 0, 0, 0, &
+       0, 1, 1, 1, 1, 1, 1, 0, &
+       0, 1, 0, 0, 0, 0, 1, 0, &
+       0, 1, 0, 0, 0, 0, 1, 0, &
+       0, 1, 0, 0, 0, 0, 1, 0, &
+       0, 1, 0, 0, 0, 0, 1, 0, &
+       0, 1, 1, 1, 1, 0, 1, 0, &
+       0, 0, 0, 0, 0, 0, 0, 0/), shape(pixels)))
+
+  do j = 1, 8
+     hi = 0
+     lo = 0
      
-     ! Write Lo-Byte
-     y = iand(1, x)
-     y = lshift(y, offset)
-     lo = ior(lo, y)
+     do i = 1, 8
+        x = pixels(j, i)
+        offset = 8 - i
 
-     ! Write Hi-Byte
-     y = rshift(x, 1)
-     y = lshift(y, offset)
-     hi = ior(hi, y)
+        ! Write Lo-Byte
+        y = iand(1, x)
+        y = lshift(y, offset)
+        lo = ior(lo, y)
 
+        ! Write Hi-Byte
+        y = rshift(x, 1)
+        y = lshift(y, offset)
+        hi = ior(hi, y)
+
+     end do
+
+     print '(z4)', lo, hi
   end do
-
-  print *, lo, hi
 
 end program main
