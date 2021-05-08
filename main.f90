7d2f7bbe8184c7236a680c02ea25caf048a32df4
Ben Hanna <benpaulhanna@gmail.com>
2021-05-08 01:18:23 -0400

    Added file reading.

--- main.f90
+++ main.f90
@@ -1,7 +1,10 @@
 program main
   implicit none
 
-  integer :: i, j, x, y, offset, hi, lo
+  integer(kind=1) :: b, x, y, hi, lo
+  integer :: seek
+  integer :: ios
+  integer :: i, j, offset
   integer, dimension(8, 8) :: pixels
   
   pixels = transpose(reshape((/0, 0, 0, 0, 0, 0, 0, 0, &
@@ -22,7 +25,7 @@ program main
         offset = 8 - i
 
         ! Write Lo-Byte
-        y = iand(1, x)
+        y = iand(int(1, 1), x)
         y = lshift(y, offset)
         lo = ior(lo, y)
 
@@ -36,4 +39,43 @@ program main
      print '(z4)', lo, hi
   end do
 
+  print *, "-----"
+
+  ios = 0
+  
+  open(1, file='chr.bmp', access='stream')
+  call fseek(1, 10, 0)
+  read(1, iostat=ios) seek
+  call fseek(1, seek, 0)
+
+  outer: do while (ios == 0)
+     hi = 0
+     lo = 0
+     
+     row: do i = 1, 8
+        read(1, iostat=ios) x
+        
+        if (ios .ne. 0) then
+           exit outer
+        end if
+
+        x = ishft(x, -6)
+        offset = 8 - i
+
+        ! Write Lo-Byte
+        y = iand(int(1, 1), x)
+        y = lshift(y, offset)
+        lo = ior(lo, y)
+
+        ! Write Hi-Byte
+        y = rshift(x, 1)
+        y = lshift(y, offset)
+        hi = ior(hi, y)
+     end do row
+
+     print '(z4,z4)', lo, hi
+  end do outer
+  
+  close(1)
+  
 end program main
